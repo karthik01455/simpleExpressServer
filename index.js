58e69b30951d527d86edfc2d4e9c20eb97208f66
karthik01455 <karthikshriram4509@gmail.com>
2023-04-20 03:51:12 +0530

    fix:

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
   console.log('Hello, World!1')
   res.send('Hello, World!1')
 })
+app.get('/colors', (req, res) => {
+  console.log('colors')
+  res.send('colors')
+})
+app.get('/user', (req, res) => {
+  console.log('user')
+  res.send('user')
+})
 
 app.listen(3000, () => {
   console.log('Express server listening on port 3000')
